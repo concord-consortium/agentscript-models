3f9f5eadbdf6377f63449a4cd2f054c2c8a77541
Richard Klancer <rpk@pobox.com>
2014-05-08 10:15:25 -0400

    Additions to script for updating gh-pages

--- update-public.sh
+++ update-public.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 cp index.html public/index.html
+cp Gemfile public/Gemfile
+cp s3_website.yml public/s3_website.yml
+cp .travis.yml public/.travis.yml
 
 for dir in "air-pollution" "air-pollution-aerial" "fracking" "global-climate" "lib" "solar-panel" "water"
 do
